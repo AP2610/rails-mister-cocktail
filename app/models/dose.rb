215b2f8a066ea1971f885a0914f7599e075897fe
Arjun Puri <arjun_261094@hotmail.com>
2019-08-15 12:52:20 +0200

    Latest edits to dose model - Updated validations

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,5 +3,7 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
 
   validates :description, presence: true
+  # You cant delete an ingredient if it is used by one single cocktail
+  # here we are saying, Validate that an ingredient
   validates :ingredient, uniqueness: { scope: :cocktail }
 end
