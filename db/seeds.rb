743ddf88e173c6298c162ce89220cf34881fcc32
Arjun Puri <arjun_261094@hotmail.com>
2019-08-17 13:03:48 +0200

    Reseeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,9 +23,8 @@ ingredients = result['drinks'].map { |ingredient| ingredient['strIngredient1'] }
   Ingredient.create(name: ingredients.sample)
 end
 
-Cocktail.create(name: 'Mojito')
-
 url = "https://unsplash.com/search/photos/mojito"
 cocktail = Cocktail.new(name: 'Mojito', photo: "A great cocktail")
 cocktail.remote_photo_url = url
 cocktail.save
+
